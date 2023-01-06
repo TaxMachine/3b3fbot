7c99aa067e5b6e29600d71348e4102d410de8f34
TaxMachine <tax.machine348@gmail.com>
2023-01-06 00:34:19 -0500

    added timestamps to seen command

--- src/commands/seen.js
+++ src/commands/seen.js
@@ -10,12 +10,15 @@ module.exports = {
         if (args.length == 1) return bot.chat(`Wrong syntax: ${this.syntax}`)
         var cleaned = await clean(args[1])
         var uuid = await username2uuid(cleaned)
-        argtable.db.get(`SELECT lastseenpos, uuid FROM playerinfo WHERE uuid = $uuid`, {
+        argtable.db.get(`SELECT lastseenpos, lastseentime, uuid FROM playerinfo WHERE uuid = $uuid`, {
             $uuid: uuid
         }, (err, row) => {
             if (err || row == null) return bot.chat("No such player")
-            var coords = JSON.parse(row.lastseenpos)
-            bot.chat(`${cleaned} was last seen at: X: ${coords.x} Y: ${coords.y} Z: ${coords.z}`)
+            var 
+                coords = JSON.parse(row.lastseenpos),
+                viewed = new Date(parseInt(row.lastseentime)),
+                textview = `${viewed.getDate()}-${viewed.getMonth()}-${viewed.getFullYear()} ${viewed.getHours()}:${viewed.getMinutes()}:${viewed.getSeconds()}`
+            bot.chat(`${cleaned} was last seen at: X: ${coords.x ? coords.x : "none"} Y: ${coords.y ? coords.y : "none"} Z: ${coords.z ? coords.z : "none"}\nLast seen Timestamp: ${textview}`)
         })
     }
 }
